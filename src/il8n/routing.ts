b91e63cda8e639d875d23dd1d6826d179e98af89
Michael Fitzgerald <michael.fitzgerald.1406@gmail.com>
2024-10-05 16:11:27 +0900

    remed il8n stuff that broke my site

--- src/il8n/routing.ts
+++ src/il8n/routing.ts
@@ -2,14 +2,10 @@ import {createLocalizedPathnamesNavigation} from 'next-intl/navigation';
 import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
-  locales: ['en', 'de'],
+  locales: ['en', 'zh', 'ko'],
   defaultLocale: 'en',
   pathnames: {
     '/': '/',
-    '/pathnames': {
-      en: '/pathnames',
-      de: '/pfadnamen'
-    }
   }
 });
 
